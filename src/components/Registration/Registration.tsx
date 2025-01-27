0526f27b6837ef35f269aceac56eb536748bd953
lns <bezborodnikovtimur@gmail.com>
2025-01-27 21:44:57 +0300

    registration name and lastName added

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -8,6 +8,8 @@ import TokenStore from "../../stores/token-store.ts";
 import { Link, useNavigate } from "react-router-dom";
 
 export const Registration: React.FC = () => {
+    const [name, setName] = useState<string>("")
+    const [lastName, setLastName] = useState<string>("")
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
     const [userName, setUserName] = useState<string>("")
@@ -20,15 +22,17 @@ export const Registration: React.FC = () => {
         const reg = async () => {
             try {
 
-                // тело запроса
+                // тело запроса (форматирование имени, фамилии)
                 const body = {
+                    name: name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(),
+                    lastName: lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase(),
                     email,
                     password,
-                    userName,
-                }
+                    userName
+                };
 
                 // проверка на пустые поля
-                if (!email.trim() || !password.trim() || !userName.trim()) {
+                if (!name.trim() || !lastName.trim() || !email.trim() || !password.trim() || !userName.trim()) {
                     return setErrorMessage("Все поля должны быть заполнены")
                 }
 
@@ -37,6 +41,8 @@ export const Registration: React.FC = () => {
                 console.log(response.data);
 
                 // очистка полей
+                setName("")
+                setLastName("")
                 setEmail("")
                 setPassword("")
                 setUserName("")
@@ -74,6 +80,8 @@ export const Registration: React.FC = () => {
                     <h3>Регистрация</h3>
                     <h4>{message}</h4>
                     <h5>{errorMessage}</h5>
+                    <LabelInput element="name" text="Имя" inputType="text" value={name} setValue={setName} />
+                    <LabelInput element="lastName" text="Фамилия" inputType="text" value={lastName} setValue={setLastName} />
                     <LabelInput element="email" text="Email" inputType="text" value={email} setValue={setEmail} />
                     <LabelInput element="password" text="Пароль" inputType="password" value={password} setValue={setPassword} />
                     <LabelInput element="userName" text="Юзернейм" inputType="text" value={userName} setValue={setUserName} />
