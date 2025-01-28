a2426456e1f8caf5f8c9e0d422d07eeba0d21ef3
lns <142323634+lnnsss@users.noreply.github.com>
2025-01-28 14:04:43 +0300

    Update Account.tsx

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,5 +1,5 @@
 import React, { ChangeEvent, useEffect, useState } from "react";
-import s from "./Account.module.css"
+import s from "./Account.module.css";
 import TokenStore from "../../stores/token-store";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
@@ -8,51 +8,91 @@ import { apiURL } from "../../configs/constants";
 
 interface PostProps {
     name: string;
-    date: string; 
+    createdAt: string; 
     text: string;
 }
-
-const posts: PostProps[] = [
-    {name: "Тимур", date: "26 января 2025", text: "in work"},
-    {name: "Тимур", date: "27 января 2025", text: "щас бы амняма из вкусноточки..."},
-    {name: "Тимур", date: "28 января 2025", text: "laptop boys 2 on the way"}
-]
+interface PostBodyProps {
+    userID: string | undefined,
+    text: string
+}
 
 export const Account: React.FC = observer(() => {
-    const [inputValue, setInputValue] = useState("");
-    const {clearToken, getID} = TokenStore
+    const [inputValue, setInputValue] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [posts, setPosts] = useState<PostProps[]>([]);
+    const { clearToken, getID } = TokenStore;
     const navigate = useNavigate();
     const id = getID();
-
+    
     useEffect(() => {
         const fetchAccountInfo = async () => {
             try {
-                const response = await axios.get(`${apiURL}/users/${id}`)
-                console.log(response.data.content);
-            } catch(err) {
-                console.error(err);                
+                const accountResponse = await axios.get(`${apiURL}/users/${id}`);
+                const postsResponse = await axios.get(`${apiURL}/posts?userID=${id}`);
+                setName(accountResponse.data.content.name);
+                
+                const fetchedPosts = postsResponse.data.content.map((post: any) => ({
+                    name,
+                    createdAt: new Date(post.createdAt).toLocaleString('ru-RU', {
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                    }),
+                    text: post.text,
+                }));
+                setPosts(fetchedPosts);
+                
+            } catch (err) {
+                console.error(err);
             }
-        }
-        fetchAccountInfo()
-    }, [])
+        };
+        fetchAccountInfo();
+    }, [id]);
 
     const handleLogOut = (): void => {
-        clearToken()
+        clearToken();
         navigate('/registration');
     }      
 
     const handleChangeInputValue = (e: ChangeEvent<HTMLInputElement>): void => {
-        setInputValue(e.target.value)
+        setInputValue(e.target.value);
     }
 
     const handleAddNewPost = (): void => {
-        const newPost = {
-            name: "Тимур",
-            date: "28 января 2025",
-            text: inputValue
+        try {
+            const addPost = async (): Promise<void> => {
+                const newPost: PostProps = {
+                    name,
+                    createdAt: new Date().toISOString(),
+                    text: inputValue
+                };
+        
+                const body: PostBodyProps = {
+                    userID: id,
+                    text: inputValue
+                }
+                const response = await axios.post(`${apiURL}/posts`, body)
+                        
+                setPosts(prevPosts => [
+                    ...prevPosts,
+                    {
+                        ...newPost,
+                            createdAt: new Date(newPost.createdAt).toLocaleString('ru-RU', {
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric'
+                            })
+                    }
+                ]);
+                
+                setInputValue("");
+                console.log(response);                
+            }
+            addPost()
+        } catch(err) {
+            console.error(err);
         }
-        posts.push(newPost)
-    }
+    };
 
     return (
         <div className={s.account}>
@@ -65,7 +105,7 @@ export const Account: React.FC = observer(() => {
                 <div className={s.account__right}>
                     <div className={s.account__header}>
                         <img className={s.account__avatar} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQncwmjK9JtQBeWuoCPkioKY3gsv4l7L7_Egw&s" alt="avatar" />
-                        <h2 className={s.account__name}>Тимур Безбородников</h2>
+                        <h2 className={s.account__name}>{name}</h2>
                         <h3 className={s.account__username}>@lnnsss</h3>
                         <h3 className={s.account__status}>almost alive</h3>
                         <h4 className={s.account__subscribes}>Подписки: 14</h4>
@@ -75,22 +115,24 @@ export const Account: React.FC = observer(() => {
                         <button onClick={handleAddNewPost}>Опубликовать</button>
                     </div>
                     <div className={s.account__posts}>
-                        {posts.map( (p, i) => <Post key={i} name={p.name} date={p.date} text={p.text} />)}
+                        {posts.map((p, i) => (
+                            <Post key={i} name={name} createdAt={p.createdAt} text={p.text} />
+                        ))}
                     </div>
                 </div>
             </div>
         </div>
-    )
-})
+    );
+});
 
-const Post = ({name, date, text}: PostProps) => {
+const Post = ({ name, createdAt, text }: PostProps) => {
     return (
         <div className={s.account__post}>
             <div className={s.account__post__header}>
                 <h3 className={s.account__post__name}>{name}</h3>
-                <h4 className={s.account__post__date}>{date}</h4>
+                <h4 className={s.account__post__date}>{createdAt}</h4>
             </div>
             <p className={s.account__post__text}>{text}</p>
         </div>
-    )
-}
+    );
+};
