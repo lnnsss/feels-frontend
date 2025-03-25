d73e21020e1b72977b2940419d9458483668d13b
lns <142323634+lnnsss@users.noreply.github.com>
2025-03-25 10:06:50 +0300

    Update Chats.tsx

--- src/components/Chats/Chats.tsx
+++ src/components/Chats/Chats.tsx
@@ -4,11 +4,18 @@ import {SideBar} from "./components/SideBar.tsx";
 import {Chat} from "./components/Chat.tsx";
 
 export const Chats: React.FC = () => {
+    const user = {
+        id: "1",
+        avatar: "https://avatars.mds.yandex.net/i?id=bdf2523f69d180816b8f1752008b521488103379-10780730-images-thumbs&n=13",
+        name: "Тимур Безбородников",
+        lastMessage: "how u feel?"
+    }
+    
     return (
         <div className={s.chats}>
             <div className={s.chats__container}>
                 <SideBar/>
-                <Chat/>
+                <Chat id={user.id} name={user.name} avatar={user.avatar} />
             </div>
         </div>
     )
