40cdc3c6bb4c4683ad1dd5c782669dbbe5c92577
lns <142323634+lnnsss@users.noreply.github.com>
2025-03-27 09:36:48 +0300

    Update Message.tsx

--- src/components/Chats/components/Message.tsx
+++ src/components/Chats/components/Message.tsx
@@ -10,7 +10,7 @@ export const Message: React.FC<MessageProps> = ({chatID, avatar, name, lastName,
             <img className={s.message__avatar} src={avatar} alt="avatar"/>
             <div className={s.message__text}>
                 <h5 className={s.message__name}>{name} {lastName}</h5>
-                <span className={s.message__lastMessage}>{lastMessage}</span>
+                <span className={s.message__lastMessage}>{lastMessage || "..."}</span>
             </div>
         </Link>
     )
