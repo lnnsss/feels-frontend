deb9263db51f86e9e1aa6b2177abb948bc4ee013
lns <142323634+lnnsss@users.noreply.github.com>
2025-03-27 10:12:37 +0300

    Update constants.ts

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,5 +1,9 @@
-export const apiURL: string = "http://localhost:3001/api";
+// API URL
+export const apiURL: string = 'http://localhost:3001/api';
 export const apiPostsURL: string = `${apiURL}/posts`;
 export const apiUsersURL: string = `${apiURL}/users`;
 export const apiAuthURL: string = `${apiURL}/auth`;
 export const apiChatsURL: string = `${apiURL}/chats`;
+
+// OTHER
+export const avatarLink: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQncwmjK9JtQBeWuoCPkioKY3gsv4l7L7_Egw&s';
