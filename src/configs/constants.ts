f56772170d5a76a4a3eae2aded3e1538651c5d2d
lns <bezborodnikovtimur@gmail.com>
2025-03-28 19:24:54 +0300

    any changes

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -1,5 +1,6 @@
-// API URL
-export const apiURL: string = 'http://localhost:3001/api';
+// SERVER URL
+export const serverURL: string = 'http://localhost:3001';
+export const apiURL: string = `${serverURL}/api`;
 export const apiPostsURL: string = `${apiURL}/posts`;
 export const apiUsersURL: string = `${apiURL}/users`;
 export const apiAuthURL: string = `${apiURL}/auth`;
